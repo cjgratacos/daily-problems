2065f192936cd3a6355184e7058eb41b542b0801
Carlos Gratacos <cjgratacos@gmail.com>
2021-11-12 17:10:46 -0500

    updates

--- 25_regex/src/main.rs
+++ 25_regex/src/main.rs
@@ -1,16 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+#![allow(dead_code)]
+
+fn main() {}
+
+fn is_match(string: &str, pattern: &str) -> bool {
+    let mut memo: Vec<Vec<bool>> = (0..string.len() + 1)
+        .map(|_| Vec::with_capacity(pattern.len() + 1))
+        .collect();
+    dp(0, 0, string, pattern, &mut memo)
 }
 
-fn is_match(text: &str, pattern: &str) -> bool {
-    let mut memo: Vec<Vec<bool>> = Vec::new();
-  protected Payment updatePayment(
-      Payment payment, IntuitEntityDataServiceProxyInterface dataServiceProxyInterface) {
-    return dataServiceProxyInterface.update(payment);
-  }
-    dp(0, 0, text, pattern, &mut memo)
+fn dp(i: usize, j: usize, s: &str, p: &str, memo: &mut Vec<Vec<bool>>) -> bool {
+    if let Some(found) = memo[i].get(j) {
+        return *found;
+    }
+
+    todo!()
 }
 
-fn dp(i:usize, j:usize, text: &str, pattern: &str, memo: &mut Vec<Vec<bool>>) -> bool {
-
+#[test]
+fn test_star_pattern() {
+    assert_eq!(is_match("aaaba", "*aba"), true);
+    assert_eq!(is_match("aaaba", "ab*"), true);
+    assert_eq!(is_match("aaaba", "*ab*"), true);
+    assert_eq!(is_match("aaaba", "*ab"), false);
 }
