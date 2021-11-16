faf7a5ba2473e74a2118c6c4d4e7b58b2d636e98
Carlos Gratacos <cjgratacos@gmail.com>
2021-11-16 14:59:18 -0500

    finishes 27, adds 29,30,31

--- 27_brackets/src/main.rs
+++ 27_brackets/src/main.rs
@@ -1,3 +1,36 @@
-fn main() {
-    println!("Hello, world!");
+fn main() {}
+
+fn is_balance(s: &str) -> bool {
+    let mut stack = Vec::new();
+
+    for c in s.chars() {
+        if c == '{' || c == '[' || c == '(' {
+            stack.push(c);
+        }
+
+        if c == '}' || c == ']' || c == ')' {
+            match stack.pop() {
+                Some(element) => {
+                    if !((element == '{' && c == '}')
+                        || (element == '(' && c == ')')
+                        || (element == '[' && c == ']'))
+                    {
+                        return false;
+                    }
+                }
+                None => {
+                    return false;
+                }
+            }
+        }
+    }
+
+    return stack.is_empty();
+}
+
+#[test]
+fn test() {
+    assert_eq!(is_balance("([])[]({})"), true);
+    assert_eq!(is_balance("([)]"), false);
+    assert_eq!(is_balance("((()"), false);
 }
