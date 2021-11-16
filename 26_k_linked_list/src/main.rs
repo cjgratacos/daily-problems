56e9fad3c47949de6a81c2513f4c03db39d52e28
Carlos Gratacos <cjgratacos@gmail.com>
2021-11-16 14:31:23 -0500

    implements linkedlist

--- 26_k_linked_list/src/main.rs
+++ 26_k_linked_list/src/main.rs
@@ -1,4 +1,3 @@
-use std::iter::IntoIterator;
 use std::sync::{Arc, Mutex};
 
 fn main() {}
@@ -6,13 +5,13 @@ fn main() {}
 type Link<T> = Option<Arc<Mutex<Box<Node<T>>>>>;
 
 #[derive(Clone, Debug)]
-pub struct Node<T> {
+pub struct Node<T: Clone> {
     pub data: T,
     pub next: Link<T>,
     pub prev: Link<T>,
 }
 
-impl<T> Node<T> {
+impl<T: Clone> Node<T> {
     pub fn new(data: T) -> Self {
         Self {
             data,
@@ -23,13 +22,18 @@ impl<T> Node<T> {
 }
 
 #[derive(Clone, Debug)]
-pub struct LinkedList<T> {
+pub struct LinkedList<T: Clone> {
     pub head: Link<T>,
     pub tail: Link<T>,
     pub size: usize,
 }
 
-impl<T> LinkedList<T> {
+#[derive(Clone, Debug)]
+pub struct NodeIterator<T: Clone> {
+    pub node: Link<T>,
+}
+
+impl<T: Clone> LinkedList<T> {
     pub fn new() -> Self {
         Self {
             head: None,
@@ -74,48 +78,150 @@ impl<T> LinkedList<T> {
                 }
             }
 
+            self.size -= 1;
             head
         })
     }
-}
 
-impl<T> IntoIterator for LinkedList<T> {
-    type Item = T;
-    type IntoIter = std::vec::IntoIter<Self::Item>;
+    pub fn remove_kth_reverse(&mut self, k: usize) -> Link<T> {
+        if self.tail.is_none() || self.size < k {
+            return None;
+        }
 
-    fn into_iter(self) -> Self::IntoIter {
-        let mut vec = vec![];
+        let mut location = 1;
+        let mut prev = self.tail.as_ref().unwrap().clone();
+        loop {
+            if location == k {
+                break;
+            }
 
-        if self.head.is_some() {
-            let next = self.head.as_ref().unwrap();
-            loop {
-                let next_lock = next.lock().unwrap();
-                vec.push(next_lock.data);
+            let p = prev;
+            let prev_lock = p.lock().unwrap();
+
+            let new_prev = prev_lock.prev.as_ref().unwrap();
+
+            prev = new_prev.clone();
+
+            location += 1;
+        }
+
+        {
+            let prev_lock = prev.lock().unwrap();
+            match &prev_lock.next {
+                // Means this is between two elements not beeing the end
+                Some(next) => {
+                    match &prev_lock.prev {
+                        // If some it is between two elements
+                        Some(prev) => {
+                            let mut next_lock = next.lock().unwrap();
+                            let mut prev_lock = prev.lock().unwrap();
+                            // Update next prev to prev
+                            let _ = std::mem::replace(&mut next_lock.prev, Some(prev.clone()));
+                            // Update prev next to next
+                            let _ = std::mem::replace(&mut prev_lock.next, Some(next.clone()));
+                        }
+                        // it is first
+                        None => {
+                            let mut next_lock = next.lock().unwrap();
+                            // Update head
+                            let _ = std::mem::replace(&mut self.head, Some(next.clone()));
+                            // Update next prev
+                            let _ = std::mem::replace(&mut next_lock.prev, None);
+                        }
+                    }
+                }
+                // Means this is the end
+                None => {
+                    match &prev_lock.prev {
+                        // True end
+                        Some(prev) => {
+                            let mut prev_lock = prev.lock().unwrap();
+                            // Update tail with new prev
+                            let _ = std::mem::replace(&mut self.tail, Some(prev.clone()));
+                            // Update prev next to empty
+                            prev_lock.next = None;
+                        }
+                        // Single Item List, now there are none
+                        None => {
+                            // Update Head to empty
+                            self.head = None;
+                            // Update Tail to empty
+                            self.tail = None;
+                        }
+                    }
+                }
             }
         }
 
-        vec.i
+        self.size -= 1;
+        Some(prev)
+    }
+
+    pub fn iter(&self) -> NodeIterator<T> {
+        NodeIterator {
+            node: self.head.clone(),
+        }
     }
 }
+
+impl<T: Clone> Iterator for NodeIterator<T> {
+    type Item = T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let mut result: Option<T> = None;
+        if let Some(current) = &self.node.clone() {
+            let current_lock = current.lock().unwrap();
+
+            result = Some(current_lock.data.clone());
+
+            match &current_lock.next {
+                Some(next) => {
+                    let _ = std::mem::replace(&mut self.node, Some(next.clone()));
+                }
+                None => {
+                    self.node = None;
+                }
+            }
+            // set node to next
+        }
+
+        result
+    }
+}
+
 #[test]
 fn test() {
     let mut list: LinkedList<usize> = LinkedList::new();
     list.push(0);
     list.push(1);
     list.push(2);
-    loop {
-        let node = list.pop();
+    list.push(3);
+    list.push(4);
+    list.push(5);
 
-        if node.is_none() {
-            break;
-        }
+    assert_eq!(list.remove_kth_reverse(2).is_some(), true);
+    assert_eq!(
+        list.iter().collect::<Vec<usize>>().eq(&vec![0, 1, 2, 3, 5]),
+        true
+    );
 
-        let n = node.as_ref().unwrap().lock().unwrap();
-        println!("{}:{:p}", &n.data, &n.data);
-        println!("n:{:p}", &n);
-        println!("next:{:p}", &n.next);
-        println!("prev:{:p}", &n.prev);
-    }
+    assert_eq!(list.remove_kth_reverse(1).is_some(), true);
+    assert_eq!(
+        list.iter().collect::<Vec<usize>>().eq(&vec![0, 1, 2, 3]),
+        true
+    );
 
-    assert_eq!(1 == 2, true);
+    assert_eq!(list.remove_kth_reverse(4).is_some(), true);
+    assert_eq!(list.iter().collect::<Vec<usize>>().eq(&vec![1, 2, 3]), true);
+
+    assert_eq!(list.remove_kth_reverse(4).is_none(), true);
+
+    assert_eq!(list.remove_kth_reverse(1).is_some(), true);
+    assert_eq!(list.iter().collect::<Vec<usize>>().eq(&vec![1, 2]), true);
+    assert_eq!(list.remove_kth_reverse(1).is_some(), true);
+    assert_eq!(list.iter().collect::<Vec<usize>>().eq(&vec![1]), true);
+    assert_eq!(list.remove_kth_reverse(1).is_some(), true);
+    assert_eq!(list.iter().collect::<Vec<usize>>().eq(&vec![]), true);
+    assert_eq!(list.remove_kth_reverse(1).is_none(), true);
+    assert_eq!(list.remove_kth_reverse(0).is_none(), true);
 }
