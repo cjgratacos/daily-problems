05566c15477ca3bf1a0c727e518bf0f3be21df05
Carlos Gratacos <cjgratacos@gmail.com>
2021-11-16 22:15:37 -0500

    finishes 29, 30 and adds 24

--- 29_letter_reduction/src/main.rs
+++ 29_letter_reduction/src/main.rs
@@ -1,3 +1,49 @@
-fn main() {
-    println!("Hello, world!");
+fn main() {}
+
+fn encode(s: &str) -> Option<String> {
+    if s.is_empty() {
+        return None;
+    }
+
+    if s.len() == 1 {
+        let s = format!("1{}", s.chars().next().unwrap());
+        return Some(s);
+    }
+
+    let mut encoded = String::new();
+    let mut counter = 0;
+    let mut last_character = None;
+    for (i, c) in s.chars().enumerate() {
+        match &last_character {
+            Some(lc) => {
+                if c == *lc {
+                    counter += 1;
+                } else {
+                    // Setup Previous
+                    encoded.push_str(&format!("{}{}", counter, lc));
+                    // Reset
+                    last_character = Some(c);
+                    counter = 1;
+                }
+                // Handle if last
+
+                if i == s.len() - 1 {
+                    encoded.push_str(&format!("{}{}", counter, c));
+                }
+            }
+            // First char
+            None => {
+                last_character = Some(c);
+                counter += 1;
+            }
+        }
+    }
+    Some(encoded)
+}
+
+#[test]
+fn test() {
+    let encoded1 = encode("AAAABBBCCDAA");
+    assert_eq!(encoded1.is_some(), true);
+    assert_eq!(encoded1.unwrap().eq("4A3B2C1D2A"), true);
 }
--- 30_water_unit/src/main.rs
+++ 30_water_unit/src/main.rs
@@ -1,3 +1,50 @@
-fn main() {
-    println!("Hello, world!");
+fn main() {}
+
+fn water(dimensions: &[usize]) -> usize {
+    let mut result = 0;
+
+    let mut left_max: usize = 0;
+    let mut right_max: usize = 0;
+
+    let mut low_index: usize = 0;
+    let mut hi_index: usize = dimensions.len() - 1;
+
+    loop {
+        println!(
+            "L:{}:{}, R:{}:{}, {}",
+            low_index, left_max, hi_index, right_max, result
+        );
+        if low_index >= hi_index {
+            break;
+        }
+
+        if dimensions[low_index] < dimensions[hi_index] {
+            if dimensions[low_index] > left_max {
+                left_max = dimensions[low_index];
+            } else {
+                result += left_max - dimensions[low_index];
+            }
+            low_index += 1;
+        } else {
+            if dimensions[hi_index] > right_max {
+                right_max = dimensions[hi_index];
+            } else {
+                result += right_max - dimensions[hi_index];
+            }
+
+            hi_index -= 1;
+        }
+    }
+
+    result
+}
+
+#[test]
+fn test() {
+    assert_eq!(water(&[2, 1, 2]) == 1, true);
+    assert_eq!(water(&[2, 0, 2]) == 2, true);
+    assert_eq!(water(&[0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]) == 6, true);
+    assert_eq!(water(&[3, 0, 2, 0, 4]) == 7, true);
+    assert_eq!(water(&[3, 0, 1, 3, 0, 5]) == 8, true);
+    assert_eq!(water(&[4, 2, 0, 3, 2, 5]) == 9, true);
 }
