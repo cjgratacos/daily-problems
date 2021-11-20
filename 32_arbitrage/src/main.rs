17f36bda8f39e1c0c510c5173422fdbbde3c163d
Carlos Gratacos <cjgratacos@gmail.com>
2021-11-19 23:11:46 -0500

    finishes 32

--- 32_arbitrage/src/main.rs
+++ 32_arbitrage/src/main.rs
@@ -1,6 +1,33 @@
 fn main() {}
 
-fn arbitrage() {}
+fn has_arbitrage(currencies: &Vec<Vec<f64>>) -> bool {
+    let size = currencies.len();
+
+    let mut memo: Vec<Vec<f64>> = currencies
+        .iter()
+        .map(|currency| currency.iter().map(|it| -it.log2()).collect())
+        .collect();
+
+    let mut ans = false;
+
+    for k in 0..size {
+        for i in 0..size {
+            for j in 0..size {
+                memo[i][j] = memo[i][j].min(memo[i][k] + memo[k][j]);
+                if memo[i][i] < 0.0 {
+                    ans = true;
+                }
+            }
+        }
+    }
+
+    return ans;
+}
 
 #[test]
-fn test() {}
+fn test() {
+    assert_eq!(has_arbitrage(&vec![]), false);
+    assert_eq!(has_arbitrage(&vec![vec![1.0]]), false);
+    assert_eq!(has_arbitrage(&vec![vec![1.0, 2.0], vec![0.5, 1.0]]), false);
+    assert_eq!(has_arbitrage(&vec![vec![1.0, 2.0], vec![0.6, 1.0]]), true);
+}
